7c459f636c382176b3d26417fbbeffa3f24491c9
Choiboi500 <jbc8894@gmail.com>
2020-09-29 13:06:32 -0400

    installed packages

---
+++ votingapp/src/Candidates.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Candidates(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Candidates;
---
+++ votingapp/src/Questions.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Questions(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Questions;
