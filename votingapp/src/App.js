2cae140a5e9221b8d6d5bba8302704ff9b194318
Leo <hamlap6@gmail.com>
2020-09-29 14:43:17 -0700

    minor changes

--- votingapp/src/App.js
+++ votingapp/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import Questions from './Questions'
+import Questions from './Questions';
+import Candidates from './Candidates';
+import About from './About';
 
 
 class App extends Component {
   render() {
     return (
       <div>
-<Questions/>
+        <Questions/>
       </div>
     );
   }
