ae205788fad6bdf6a27288f7bc94c0f7654ef23b
jamesnascimento1994 <james.nascimento94@yahoo.com>
2020-09-30 12:22:07 -0400

    in the process of adding question functionality

--- votingapp/src/Questions.js
+++ votingapp/src/Questions.js
@@ -1,174 +1,200 @@
-import React,{useState} from 'react';
-import Carousel from 'react-bootstrap/Carousel'
-
-
+import React,{ useState, useRef } from 'react';
+import {Carousel, Button, ButtonGroup} from 'react-bootstrap'
 function Questions(props) {
-const [userScore, setUserScore] = useState(0); 
-const [userCandi, setUserCandi] = useState(' '); 
+    let btnRef = useRef()
+    const [userScore, setUserScore] = useState(0); 
+    const [userCandi, setUserCandi] = useState(' '); 
 
-    const trumpScore = -7;
-    const harrisScore = -2;
-    // const penceScore = 0;
-    // const bidenScore = 5;
+    const trumpScore = -9.5;
+    const harrisScore = 9.5;
+    const penceScore = -10;
+    const bidenScore = 7;
+    console.log(btnRef)
     
-
-    const handleClickYes = () => {
-        setUserScore(userScore + 1) 
-    }
-    const handleClickNo = () => {
-        setUserScore(userScore - 1) 
+const handleClickYes = (e) => {
+    setUserScore(userScore + 1)
+    if(btnRef.current){
+btnRef.current.setAttribute("disabled", "disabled");
+}
+}
+const handleClickNo = () => {
+    setUserScore(userScore - 1)
+    if (btnRef.current) {
+			btnRef.current.setAttribute('disabled', 'disabled');
+		} 
     }
     const handleClickMaybe = () => {
-        setUserScore(userScore) 
-    }     
+        setUserScore(userScore)
+        if (btnRef.current) {
+					btnRef.current.setAttribute('disabled', 'disabled');
+				} 
+    }
+
      const handleClick = () =>{
-         if(userScore>=trumpScore && userScore<harrisScore){
-            setUserCandi('trump')
+         if(userScore>=penceScore && userScore<trumpScore){
+            setUserCandi('Pence')
+        } else if (userScore >= trumpScore && userScore < 1.25) {
+            setUserCandi('Trump')
+        } else if (userScore>=1.25 && userScore<=bidenScore) {
+            setUserCandi('Biden')
+        } else {
+            setUserCandi('Harris')
         }
     }
+    console.log(userCandi)
+    console.log(userScore)
 
     // can create an array with all the questions and map through it, returning carousel.item's for cleaner code
     return (
-        <div>
-            <div>
-            <Carousel interval={null} >
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-                    alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>First Question</h3>
-                        <button onClick={handleClickYes}>Yes</button>
-                        <button onClick={handleClickNo}>No</button>
-                        <button onClick={handleClickMaybe}>Maybe</button>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-                    alt="Third slide"
-                    />
+			<div>
+				<div>
+					<Carousel interval={null}>
+						<Carousel.Item>
+							<img
+								className='d-block w-100'
+								src='https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
+								alt='First slide'
+							/>
+							<Carousel.Caption>
+								<h3>First Question</h3>
+                                <ButtonGroup>
 
-                    <Carousel.Caption>
-                        <h3>First Question</h3>
-                        <button onClick={handleClickYes}>Yes</button>
-                        <button onClick={handleClickNo}>No</button>
-                        <button onClick={handleClickMaybe}>Maybe</button>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-                    alt="Third slide"
-                    />
+								<Button onClick={handleClickYes} ref={btnRef}>
+									Yes
+								</Button>
+								<Button onClick={handleClickNo} ref={btnRef}>
+									No
+								</Button>
+								<Button onClick={handleClickMaybe} ref={btnRef}>
+									Maybe
+								</Button>
+                                </ButtonGroup>
+							</Carousel.Caption>
+						</Carousel.Item>
+						<Carousel.Item>
+							<img
+								className='d-block w-100'
+								src='https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
+								alt='Third slide'
+							/>
 
-                    <Carousel.Caption>
-                        <h3>First Question</h3>
-                        <button onClick={handleClickYes}>Yes</button>
-                        <button onClick={handleClickNo}>No</button>
-                        <button onClick={handleClickMaybe}>Maybe</button>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-                    alt="Third slide"
-                    />
+							<Carousel.Caption>
+								<h3>First Question</h3>
+								<button onClick={handleClickYes}>Yes</button>
+								<button onClick={handleClickNo}>No</button>
+								<button onClick={handleClickMaybe}>Maybe</button>
+							</Carousel.Caption>
+						</Carousel.Item>
+						<Carousel.Item>
+							<img
+								className='d-block w-100'
+								src='https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
+								alt='Third slide'
+							/>
 
-                    <Carousel.Caption>
-                        <h3>First Question</h3>
-                        <button onClick={handleClickYes}>Yes</button>
-                        <button onClick={handleClickNo}>No</button>
-                        <button onClick={handleClickMaybe}>Maybe</button>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-                    alt="Third slide"
-                    />
+							<Carousel.Caption>
+								<h3>First Question</h3>
+								<button onClick={handleClickYes}>Yes</button>
+								<button onClick={handleClickNo}>No</button>
+								<button onClick={handleClickMaybe}>Maybe</button>
+							</Carousel.Caption>
+						</Carousel.Item>
+						<Carousel.Item>
+							<img
+								className='d-block w-100'
+								src='https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
+								alt='Third slide'
+							/>
 
-                    <Carousel.Caption>
-                        <h3>First Question</h3>
-                        <button onClick={handleClickYes}>Yes</button>
-                        <button onClick={handleClickNo}>No</button>
-                        <button onClick={handleClickMaybe}>Maybe</button>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-                    alt="Third slide"
-                    />
+							<Carousel.Caption>
+								<h3>First Question</h3>
+								<button onClick={handleClickYes}>Yes</button>
+								<button onClick={handleClickNo}>No</button>
+								<button onClick={handleClickMaybe}>Maybe</button>
+							</Carousel.Caption>
+						</Carousel.Item>
+						<Carousel.Item>
+							<img
+								className='d-block w-100'
+								src='https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
+								alt='Third slide'
+							/>
 
-                    <Carousel.Caption>
-                        <h3>First Question</h3>
-                        <button onClick={handleClickYes}>Yes</button>
-                        <button onClick={handleClickNo}>No</button>
-                        <button onClick={handleClickMaybe}>Maybe</button>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-                    alt="Third slide"
-                    />
+							<Carousel.Caption>
+								<h3>First Question</h3>
+								<button onClick={handleClickYes}>Yes</button>
+								<button onClick={handleClickNo}>No</button>
+								<button onClick={handleClickMaybe}>Maybe</button>
+							</Carousel.Caption>
+						</Carousel.Item>
+						<Carousel.Item>
+							<img
+								className='d-block w-100'
+								src='https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
+								alt='Third slide'
+							/>
 
-                    <Carousel.Caption>
-                        <h3>First Question</h3>
-                        <button onClick={handleClickYes}>Yes</button>
-                        <button onClick={handleClickNo}>No</button>
-                        <button onClick={handleClickMaybe}>Maybe</button>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-                    alt="Third slide"
-                    />
+							<Carousel.Caption>
+								<h3>First Question</h3>
+								<button onClick={handleClickYes}>Yes</button>
+								<button onClick={handleClickNo}>No</button>
+								<button onClick={handleClickMaybe}>Maybe</button>
+							</Carousel.Caption>
+						</Carousel.Item>
+						<Carousel.Item>
+							<img
+								className='d-block w-100'
+								src='https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
+								alt='Third slide'
+							/>
 
-                    <Carousel.Caption>
-                        <h3>First Question</h3>
-                        <button onClick={handleClickYes}>Yes</button>
-                        <button onClick={handleClickNo}>No</button>
-                        <button onClick={handleClickMaybe}>Maybe</button>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-                    alt="Third slide"
-                    />
+							<Carousel.Caption>
+								<h3>First Question</h3>
+								<button onClick={handleClickYes}>Yes</button>
+								<button onClick={handleClickNo}>No</button>
+								<button onClick={handleClickMaybe}>Maybe</button>
+							</Carousel.Caption>
+						</Carousel.Item>
+						<Carousel.Item>
+							<img
+								className='d-block w-100'
+								src='https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
+								alt='Third slide'
+							/>
 
-                    <Carousel.Caption>
-                        <h3>First Question</h3>
-                        <button onClick={handleClickYes}>Yes</button>
-                        <button onClick={handleClickNo}>No</button>
-                        <button onClick={handleClickMaybe}>Maybe</button>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-                    alt="Third slide"
-                    />
-                    <Carousel.Caption>
-                        <button onClick={handleClick}>Who's my Candi?</button>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-                {/* <ul>
+							<Carousel.Caption>
+								<h3>First Question</h3>
+								<button onClick={handleClickYes}>Yes</button>
+								<button onClick={handleClickNo}>No</button>
+								<button onClick={handleClickMaybe}>Maybe</button>
+							</Carousel.Caption>
+						</Carousel.Item>
+						<Carousel.Item>
+							<img
+								className='d-block w-100'
+								src='https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
+								alt='Third slide'
+							/>
+
+							<Carousel.Caption>
+								<h3>First Question</h3>
+								<button onClick={handleClickYes}>Yes</button>
+								<button onClick={handleClickNo}>No</button>
+								<button onClick={handleClickMaybe}>Maybe</button>
+							</Carousel.Caption>
+						</Carousel.Item>
+						<Carousel.Item>
+							<img
+								className='d-block w-100'
+								src='https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
+								alt='Third slide'
+							/>
+							<Carousel.Caption>
+								<button onClick={handleClick}>Who's my Candi?</button>
+							</Carousel.Caption>
+						</Carousel.Item>
+					</Carousel>
+					{/* <ul>
                     <li>
                         Question1
                         
@@ -229,11 +255,9 @@ const [userCandi, setUserCandi] = useState(' ');
                     
 
                 </ul> */}
-            </div>
-
-
-            </div>
-    );
+				</div>
+			</div>
+		);
 }
 
 export default Questions;
