7813d03ba1e42463e5572623986f405f06d79562
Leo <hamlap6@gmail.com>
2020-09-30 15:39:36 -0700

    populated candidate data, missing img links

---
+++ votingapp/src/data/candidateData.js
@@ -0,0 +1,28 @@
+const candidateData = [
+    {
+        name: 'Joe Biden',
+        img: null,
+        blurb: 'Biden is the former Vice President under the Obama Administration. He is currently running  on the legacy of the eight years he served alongside President Barack Obama and has proposed advancing that legacy on key issues like health care and the climate crisis. When he formally announced his entry into the 2020 presidential race, Joe Biden declared that he stood for two things - workers who "built this country", and values that can bridge its divisions. Some of the issues his platform advocates for include $15 minimum hourly wage, expansion of Obamacare subsidies, and free college for families making under $125,000 a year.',
+        website: 'https://joebiden.com/joes-vision/'
+    },
+    {
+        name: 'Dumb Fuck 1',
+        img: null,
+        blurb: 'Donald Trump is the current president of the United States. He was elected the 45th president in 2016. He did not win the popular vote. It is difficult to define what his views are on any particular issue because he is constantly changing his statements and contradicting himself. He has had to file for bankruptcy six times. A former professor of his at Wharton School of Business once said that "Donald Trump was the dumbest goddamn student I ever had." He has dodged the draft five times, and he is known to cheat at golf.',
+        website: 'https://www.donaldjtrump.com/'
+    },
+    {
+        name: 'Dumb Fuck 2',
+        img: null,
+        blurb: 'Mike Pence Is the current Vice President. He was elected alongside Donald Trump in 2016. He does not beleive that same-sex couples have the right to marry and has opposed federal funding that would support treatment for people suffering from H.I.V. and AIDS, unless the government simultaneously invests in programs to discourage people from engaging in same-sex relationships. He is against regulations to limit carbon emissions.',
+        website: 'https://www.whitehouse.gov/people/mike-pence/'
+    },
+    {
+        name: 'Kamala Harris',
+        img: null,
+        blurb: `Kamala D. Harris is the second African-American woman and first South Asian-American senator in history. After earning an undergraduate degree from Howard University and a law degree from the University of California, Hastings, she began her career in the Alameda County District Attorney's Office. In 2003, Kamala became the District Attorney of San Fransisco county and, after completing two terms, was elected as the first African-American and first woman to serve as CA's Attorney General. While she didn't win the democratic candidacy for President in 2020, she was selected as Joe Biden's running-mate for the election to be held this November. She is a self-described "progressive prosecutor", and believes she could best change the criminal justice system from within.`,
+        website: 'https://joebiden.com/together-we-will-beat-donald-trump/'
+    }
+]
+
+export default candidateData;
