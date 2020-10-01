3d027fab077693d4ed43f326a31315a71887e828
Leo <hamlap6@gmail.com>
2020-10-01 09:29:49 -0700

    fixed errors

--- votingapp/src/Match.js
+++ votingapp/src/Match.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './CSS/Match.css'
 
-import Nav from './navbar/Nav'
-
 
 function Match(props) {
     return (
@@ -13,9 +11,8 @@ function Match(props) {
                 <img className='homeNav' src='https://user-images.githubusercontent.com/65630204/94823954-73751300-03d2-11eb-947d-4ede058d4160.png' />
 
             </div> */}
-            <Nav/>
 
-            <div className='match'>It's a Match!</div>
+            <div className='matchMessage'>It's a Match!</div>
             <img className='matchImg'src='https://user-images.githubusercontent.com/65630204/94753361-06269b00-035c-11eb-90dc-e9031fc83a28.png' alt='' />
             <div className='percentDisplay'>90%</div>
             <div className='name'>Joe Biden</div>
