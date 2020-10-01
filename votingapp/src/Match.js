9d7f31a5ddbbfc6f0ecf823e31d058617a6da051
Choiboi500 <jbc8894@gmail.com>
2020-09-30 20:35:37 -0400

    candidate data update

---
+++ votingapp/src/Match.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Match(props) {
+    return (
+        <div className='matchPage'>
+            <h1>It's a Match!</h1>
+            
+        </div>
+    );
+}
+
+export default Match;
